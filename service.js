4168aa0c9f755aa56271556270b6ad0a33d1fffb
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2025-06-01 11:05:57 +0200

    service

---
+++ service.js
@@ -0,0 +1,39 @@
+const Service = require('node-windows').Service
+const path = require('path')
+
+const scriptPath = path.join(__dirname, 'server.js')
+
+const svc = new Service({
+  name: 'ChoiceNestService',
+  description: 'ChoiceNest NodeJS Service',
+  script: scriptPath,
+  env: [
+    {
+      name: "NODE_ENV",
+      value: "production"
+    }
+  ]
+})
+
+svc.on('install', () => {
+  console.log('Service installed')
+  svc.start()
+})
+
+svc.on('alreadyinstalled', () => {
+  console.log('Service already installed')
+})
+
+svc.on('start', () => {
+  console.log('Service started')
+})
+
+svc.on('stop', () => {
+  console.log('Service stopped')
+})
+
+svc.on('error', (err) => {
+  console.error('Service error:', err)
+})
+
+svc.install()
